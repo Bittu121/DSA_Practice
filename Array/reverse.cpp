efde7dd9022b94201d0073ed122285b71d4ade52
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-09-20 18:31:30 +0530

    Add files via upload

---
+++ Array/reverse.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int reverseOfArray(int*arr,int n){
+	int i=0;
+	int j=n-1;
+	while(i<=j){
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j]=temp;
+		i++;
+		j--;
+	}
+}
+int main(){
+	int n;
+	cin>>n;
+	int *arr=new int[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	cout<<reverseOfArray(arr,n);
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	delete[] arr;
+}
