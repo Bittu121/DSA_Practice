cd5010cbd7077623bce6dc667a2faebdf5de5efe
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-09-23 20:40:11 +0530

    Add files via upload

---
+++ Array/populate.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int PopulateArray(int *arr,int n){
+	int i=0;
+	int j=n-1;
+	int count = 1;
+	while(i<=j){
+		if(count%2==1){
+			arr[i]=count;
+			count++;
+			i++;
+		}
+		else{
+			arr[j]=count;
+			count++;
+			j--;
+		}	
+	}
+}
+int main(){
+	int n;
+	cout<<"enter a number:"<<endl;
+	cin>>n;
+	int *arr = new int[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	cout<<"Populate Array :"<<PopulateArray(arr,n);
+	cout<<endl;
+	delete[] arr;
+}
+
