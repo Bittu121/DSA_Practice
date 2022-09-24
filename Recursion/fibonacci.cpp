7daaf32dec6cb8410100fa18867a05106a0970e1
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-09-24 19:07:25 +0530

    Update fibonacci.cpp

--- Recursion/fibonacci.cpp
+++ Recursion/fibonacci.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
 using namespace std;
 int fibonacci(int n){
-	
+	if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+        int ans=fib(n-1)+fib(n-2);
+        return ans;
 }
 int main(){
 	int n;
