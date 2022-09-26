5517f8f7de7c37cc8117b2bbc60625092e909679
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-09-26 20:24:06 +0530

    Add files via upload

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+	//Base case
+	if(n==0)
+	return 1;
+	//Recursion call
+	//int smalloutput = factorial(n-1);
+	//small calculation
+	//return n*smalloutput;
+	return n*factorial(n-1);
+}
+int main(){
+	int n;
+	cout<<"Enter number:"<<endl;
+	cin>>n;
+	cout<<"Factorial:"<<factorial(n);
+}
