43ca534e91b3c94f56a9d4cba97233f83019659c
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-10-03 20:07:37 +0530

    Update valid perfectSquare(BinarySearch).cpp

--- Array/valid perfectSquare(BinarySearch).cpp
+++ Array/valid perfectSquare(BinarySearch).cpp
@@ -1,3 +1,8 @@
+/*
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+Follow up: Do not use any built-in library function such as sqrt.
+*/
+
 #include<iostream>
 using namespace std;
 bool isPerfectSquare(int num) {
