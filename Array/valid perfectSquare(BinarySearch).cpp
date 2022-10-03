4fa10bcb1d7a5226bdcf13fd2fc35239e00bf43a
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-10-03 20:06:41 +0530

    Add files via upload

---
+++ Array/valid perfectSquare(BinarySearch).cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+bool isPerfectSquare(int num) {
+        int l=1;
+        int r=(l<<31-1);
+        while(l<r){
+            int mid=(l+r)/2;
+            long long ans=(long long)mid*mid;
+            if(ans==(long long)num)
+                return true;
+            else if(ans>(long long)num)
+                r=mid-1;
+            else
+                l=mid+1;
+        }
+        if((long long)l*l==(long long)num)
+            return true;
+        return false;
+    }
+int main(){
+    int num;
+    cin>>num;
+    isPerfectSquare(num);
+}
