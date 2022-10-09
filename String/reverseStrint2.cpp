fb4c846239a077d2a0e8f391633c67f4a7999ca8
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-10-09 14:22:06 +0530

    Add files via upload

---
+++ String/reverseStrint2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+string reverseStr(string s, int k) {
+    int n = s.size();
+    for(int i=0;i<n;i+=2*k){
+        reverse(s.begin()+i,s.begin()+i+min(k,n-i));
+        
+    }
+    
+    return s;
+    
+}
+int main(){
+    
+}
