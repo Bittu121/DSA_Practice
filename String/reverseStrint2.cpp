e4af39277db42e47968e0ced76d955b41ded338f
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-10-09 14:23:28 +0530

    Update reverseStrint2.cpp

--- String/reverseStrint2.cpp
+++ String/reverseStrint2.cpp
@@ -1,3 +1,10 @@
+/*
+Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
+If there are fewer than k characters left, reverse all of them. If there are less than 2k but
+greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+*/
+
+
 #include<iostream>
 using namespace std;
 string reverseStr(string s, int k) {
@@ -5,10 +12,8 @@ string reverseStr(string s, int k) {
     for(int i=0;i<n;i+=2*k){
         reverse(s.begin()+i,s.begin()+i+min(k,n-i));
         
-    }
-    
-    return s;
-    
+    } 
+    return s;  
 }
 int main(){
     
