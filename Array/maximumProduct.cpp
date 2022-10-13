0828cfa2120d629cbee5e1d2042132defd75d817
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-10-13 20:32:30 +0530

    Update maximumProduct.cpp

--- Array/maximumProduct.cpp
+++ Array/maximumProduct.cpp
@@ -1,3 +1,6 @@
+/*
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+*/
 #include<iostream>
 #include<vector>
 int maximumProduct(vector<int>& nums) {
