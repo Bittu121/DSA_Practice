06a3c23ba6f88426cabef3357effc70efc1f11b4
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-10-14 22:06:01 +0530

    Add files via upload

---
+++ DynamicProgramming/Lcs.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+int Lcs(int i,int j,string &s1,string &s2,vector<vector<int>> &dp){
+    if(i < 0 or j < 0)
+    return 0;
+    if(dp[i][j]!=-1)
+    return dp[i][j];
+    if(s1[i] == s2[j])
+    return dp[i][j] = Lcs(i-1,j-1,s1,s2,dp) + 1;
+    else
+    return dp[i][j] = max(Lcs(i-1,j,s1,s2,dp),Lcs(i,j-1,s1,s2,dp));
+}
+int longestCommonSubsequence(string text1, string text2) {
+    int i = text1.size();
+    int j = text2.size();
+    vector<vector<int>> dp(i+1,vector<int> (j+1,-1));
+    Lcs(i,j,text1,text2,dp);
+    return dp[i-1][j-1];
+}
+
+int main(){
+    
+}
