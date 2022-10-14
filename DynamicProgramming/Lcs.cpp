eef2d78c3a8b85ddac679a8be69c47d4eef39c51
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-10-14 22:06:52 +0530

    Update Lcs.cpp

--- DynamicProgramming/Lcs.cpp
+++ DynamicProgramming/Lcs.cpp
@@ -1,3 +1,10 @@
+/*
+Given two strings text1 and text2, return the length of their longest common subsequence.
+If there is no common subsequence, return 0
+*/
+
+
+
 #include<iostream>
 using namespace std;
 #include<vector>
