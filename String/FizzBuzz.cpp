da07aa9cceac970dc8eae68f53c495013ac065ad
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-10-15 22:10:44 +0530

    Update FizzBuzz.cpp

--- String/FizzBuzz.cpp
+++ String/FizzBuzz.cpp
@@ -7,6 +7,9 @@ answer[i] == "Buzz" if i is divisible by 5.
 answer[i] == i (as a string) if none of the above conditions are true.
 Input: n = 3
 Output: ["1","2","Fizz"]
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
 */
 
 
