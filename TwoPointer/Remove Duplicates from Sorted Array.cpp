572c4ca60eab4df932d5f512cd0e1778739e4bf0
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-10-18 20:27:27 +0530

    Update Remove Duplicates from Sorted Array.cpp

--- TwoPointer/Remove Duplicates from Sorted Array.cpp
+++ TwoPointer/Remove Duplicates from Sorted Array.cpp
@@ -1,3 +1,11 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that 
+each unique element appears only once. The relative order of the elements should be kept the same.
+
+
+*/
+
+
 #include<iostream>
 using namespace std;
 #include<vector>;
