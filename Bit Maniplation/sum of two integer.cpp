d178486c2785f90fea0f8365b6251faf7b5fb500
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-10-21 21:12:39 +0530

    Add files via upload

---
+++ Bit Maniplation/sum of two integer.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+ int getSum(int a, int b) {
+     int ans = 0;
+     bool x1 = 0, y1 = 0, carry = 0;
+     // iterate through 32 bits
+     for (int c = 0; c < 32; c++)
+        {
+            // get the last bit
+            x1 = a & 1;
+            y1 = b & 1;
+
+            // get result of adding two bits and any carry over
+            ans = (x1^ y1 ^ carry) << c | ans;
+
+            // calculate the new carry over (when atleast two bits are 1)
+            carry = (x1 | y1) & (carry | x1) & (carry | y1);
+
+            // truncate one bit
+            a >>= 1;
+            b >>= 1;
+        }
+
+        return ans; 
+    }
+    int main(){
+        int a,b;
+        cin>>a>>b;
+        cout<<getSum(a,b);
+    }
