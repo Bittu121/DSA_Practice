626c843ca12fd8f4fbbc01a77e585a6995cbc223
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-10-29 23:13:49 +0530

    Add files via upload

---
+++ String/validPalindrome.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+#include<cstring>
+bool isPalindrome(string s) {
+    int start=0,end=s.length()-1;
+    while(start<=end){
+        if(!isalnum(s[start])){
+            start++;
+            continue;
+        }
+        if(!isalnum(s[end])){ //isalnum to check whether its alphabet or number
+
+            end--;
+            continue;
+        }
+        if(tolower(s[start])!=tolower(s[end])){ //tolower to cover it into lower case incase its case sensitiv
+            return 0;
+        }
+        else{
+            start++;
+            end--;
+        }
+    }
+    return 1;
+}
+int main(){
+    string s;
+    cin>>s;
+    bool ans=isPalindrome(s);
+    cout<<ans<<endl;
+}
