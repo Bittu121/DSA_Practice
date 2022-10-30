6df10b31fbf94f540512d69fbfd27869e4c6a2c4
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-10-30 21:28:43 +0530

    Update IntersectionOfTwoArray.cpp

--- Sorting/IntersectionOfTwoArray.cpp
+++ Sorting/IntersectionOfTwoArray.cpp
@@ -3,6 +3,8 @@ Given two integer arrays nums1 and nums2, return an array of their intersection.
 Input: nums1 = [1,2,2,1], nums2 = [2,2]
 Output: [2]
 
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
 */
 #include<iostream>
 #include<vector>
