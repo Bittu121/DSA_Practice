4dd0fbcb6101d468d359fe637fd38168d0130d0b
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-10-31 20:21:17 +0530

    Update how many numbers are smaller than the current number.cpp

--- Sorting/how many numbers are smaller than the current number.cpp
+++ Sorting/how many numbers are smaller than the current number.cpp
@@ -3,6 +3,8 @@ Given the array nums, for each nums[i] find out how many numbers in the array ar
 That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
 Return the answer in an array.
 
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
 */
 
 
