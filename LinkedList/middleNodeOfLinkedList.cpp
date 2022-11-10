e44d81d5f0e769b5c7a6f6631abd6ad64f1935f1
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-11-10 19:47:23 +0530

    Update middleNodeOfLinkedList.cpp

--- LinkedList/middleNodeOfLinkedList.cpp
+++ LinkedList/middleNodeOfLinkedList.cpp
@@ -1,12 +1,7 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+/*
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+
  */
 class Solution {
 public:
