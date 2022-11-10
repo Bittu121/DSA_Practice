dc352dc2b0a89d377d84fc7fa1ddc3eb2881cd69
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-11-10 19:47:57 +0530

    Update middleNodeOfLinkedList.cpp

--- LinkedList/middleNodeOfLinkedList.cpp
+++ LinkedList/middleNodeOfLinkedList.cpp
@@ -1,7 +1,8 @@
 /*
 Given the head of a singly linked list, return the middle node of the linked list.
 If there are two middle nodes, return the second middle node.
-
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
  */
 class Solution {
 public:
