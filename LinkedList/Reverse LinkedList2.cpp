dd4b6df1d52df0feca99c79595291f4bd916ef85
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-11-12 02:31:04 +0530

    Update Reverse LinkedList2.cpp

--- LinkedList/Reverse LinkedList2.cpp
+++ LinkedList/Reverse LinkedList2.cpp
@@ -1,12 +1,6 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+/*
+ Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+
  */
 class Solution {
 public:
