3b1e79db6938eb0dcdcb29a2a22b33d158b8f6c2
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-11-14 20:29:43 +0530

    Update MergeTwoSortedLinkedList.cpp

--- LinkedList/MergeTwoSortedLinkedList.cpp
+++ LinkedList/MergeTwoSortedLinkedList.cpp
@@ -4,6 +4,9 @@ Merge the two lists in a one sorted list. The list should be made by splicing to
 Return the head of the merged linked list.
 Input: list1 = [1,2,4], list2 = [1,3,4]
 Output: [1,1,2,3,4,4]
+Input: list1 = [], list2 = []
+Output: []
+
 */
 /**
  * Definition for singly-linked list.
