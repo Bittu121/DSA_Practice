b62a20c0d2d601a8af445dc5d91b6e08d21fa682
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-11-17 19:47:32 +0530

    Add files via upload

---
+++ math/Base7.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+string convertToBase7(int num){
+    if (num < 0) return "-" + convertToBase7(-num);
+    if (num < 7) return to_string(num);
+    return convertToBase7(num / 7) + to_string(num % 7);
+}
+int main(){
+    int num;
+    cin>>num;
+    cout<<convertToBase7(num)<<endl;
+    return 0;
+}   
