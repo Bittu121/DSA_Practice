9b02b8484bdfe60d0d6fb83f24aab24c48c41c55
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-11-18 21:08:27 +0530

    Add files via upload

---
+++ LinkedList/increment.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node*next;
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+void increment(Node*head){
+    Node*temp=head;
+    while(temp!=NULL){
+        temp->data++;
+        temp=temp->next;
+        
+    }
+}
+void print(Node*head){
+    Node*temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+int main()
+{
+    //Dynamically
+    Node*n1=new Node(10);
+    Node*n2=new Node(20);
+    Node*n3=new Node(30);
+    Node*n4=new Node(40);
+    Node*n5=new Node(50);
+    Node*head=n1;
+    n1->next=n2;
+    n2->next=n3;
+    n3->next=n4;
+    n4->next=n5;
+    increment(head);
+    print(head);
+}
