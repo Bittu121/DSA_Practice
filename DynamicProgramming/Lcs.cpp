78cef120c3868e020a5a3e76fe5b729417881d96
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-11-21 19:38:04 +0530

    Update Lcs.cpp

--- DynamicProgramming/Lcs.cpp
+++ DynamicProgramming/Lcs.cpp
@@ -11,7 +11,6 @@ A common subsequence of two strings is a subsequence that is common to both stri
 
 Input: text1 = "abcde", text2 = "ace" 
 Output: 3  
-Explanation: The longest common subsequence is "ace" and its length is 3.
  */
 */
 
