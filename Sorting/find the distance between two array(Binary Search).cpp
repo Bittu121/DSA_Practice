212b4c1aa5812a56e25042b611df88a3f64bf5b9
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-11-23 20:28:41 +0530

    Update find the distance between two array(Binary Search).cpp

--- Sorting/find the distance between two array(Binary Search).cpp
+++ Sorting/find the distance between two array(Binary Search).cpp
@@ -2,6 +2,8 @@
 Given two integer arrays arr1 and arr2, and the integer d, return the distance value between the two arrays.
 
 The distance value is defined as the number of elements arr1[i] such that there is not any element arr2[j] where |arr1[i]-arr2[j]| <= d.
+Input: arr1 = [4,5,8], arr2 = [10,9,1,8], d = 2
+Output: 2
 */
 #include<iostream>
 #include<vector>
