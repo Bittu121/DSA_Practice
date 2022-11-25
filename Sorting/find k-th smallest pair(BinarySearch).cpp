b7c5d18c03c6c803e4807e15a25c51983dd30b94
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-11-25 22:20:59 +0530

    Update find k-th smallest pair(BinarySearch).cpp

--- Sorting/find k-th smallest pair(BinarySearch).cpp
+++ Sorting/find k-th smallest pair(BinarySearch).cpp
@@ -2,7 +2,8 @@
 The distance of a pair of integers a and b is defined as the absolute difference between a and b.
 
 Given an integer array nums and an integer k, return the kth smallest distance among all the pairs nums[i] and nums[j] where 0 <= i < j < nums.length.
-
+Input: nums = [1,3,1], k = 1
+Output: 0
  
 */
 #include<iostream>
