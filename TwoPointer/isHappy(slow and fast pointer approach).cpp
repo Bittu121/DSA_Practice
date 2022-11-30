89eb154ba359cbe71fd63ac9ae479c694f503fb6
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-11-30 21:37:06 +0530

    Update isHappy(slow and fast pointer approach).cpp

--- TwoPointer/isHappy(slow and fast pointer approach).cpp
+++ TwoPointer/isHappy(slow and fast pointer approach).cpp
@@ -1,6 +1,5 @@
 /*
 Write an algorithm to determine if a number n is happy.
-
 A happy number is a number defined by the following process:
 
 Starting with any positive integer, replace the number by the sum of the squares of its digits.
