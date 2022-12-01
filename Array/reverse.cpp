6b90b494eef5d661b49d52f6ce6047fb509a1a11
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-12-01 21:15:25 +0530

    Update reverse.cpp

--- Array/reverse.cpp
+++ Array/reverse.cpp
@@ -2,6 +2,8 @@
 Given an array the task is to reverse the array.
 Input  : arr[] = {1, 2, 3}
 Output : arr[] = {3, 2, 1}
+Input :  arr[] = {4, 5, 1, 2}
+Output : arr[] = {2, 1, 5, 4}
 */
 #include<iostream>
 using namespace std;
