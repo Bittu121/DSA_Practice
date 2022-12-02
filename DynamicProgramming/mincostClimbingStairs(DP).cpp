aa3d2b9518b3a699ea324c56d3cb230662876dfa
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-12-02 20:08:39 +0530

    Update mincostClimbingStairs(DP).cpp

--- DynamicProgramming/mincostClimbingStairs(DP).cpp
+++ DynamicProgramming/mincostClimbingStairs(DP).cpp
@@ -3,6 +3,8 @@ You are given an integer array cost where cost[i] is the cost of ith step on a s
 Once you pay the cost, you can either climb one or two steps.
 You can either start from the step with index 0, or the step with index 1
 Return the minimum cost to reach the top of the floor.
+Input: n = 2
+Output: 2
 */
 
 #include<iostream>
