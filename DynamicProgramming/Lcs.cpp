f1a32d88d031018cd0a732664c00862bb323c9c9
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-12-02 20:07:19 +0530

    Update Lcs.cpp

--- DynamicProgramming/Lcs.cpp
+++ DynamicProgramming/Lcs.cpp
@@ -1,5 +1,4 @@
 /*
-/*
 Given two strings text1 and text2, return the length of their longest common subsequence.
 If there is no common subsequence, return 0
 
@@ -12,7 +11,6 @@ A common subsequence of two strings is a subsequence that is common to both stri
 Input: text1 = "abcde", text2 = "ace" 
 Output: 3  
  */
-*/
 
 #include<iostream>
 using namespace std;
