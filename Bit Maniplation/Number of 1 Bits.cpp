1d3be392bb123d2ded0b66fbed07a00765ec2aec
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-12-03 19:50:39 +0530

    Add files via upload

---
+++ Bit Maniplation/Number of 1 Bits.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int hammingWeight(uint32_t n) {
+    int count = 0;
+    while(n){
+        n&=n-1;
+        count++;
+    }
+    return count;
+}
+int main(){
+    
+}
