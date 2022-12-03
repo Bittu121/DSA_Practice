6f64a667dbbae2961db7d7693a9c0dcfaa4effbb
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2022-12-03 19:51:53 +0530

    Update Number of 1 Bits.cpp

--- Bit Maniplation/Number of 1 Bits.cpp
+++ Bit Maniplation/Number of 1 Bits.cpp
@@ -1,6 +1,7 @@
 /*
 Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
 Note
+Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
 */
 #include<iostream>
 using namespace std;
