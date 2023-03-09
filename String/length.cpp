9d090c3af6b9dad9938811c134b86d2a5c93275e
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2023-03-09 22:23:34 +0530

    Update length.cpp

--- String/length.cpp
+++ String/length.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+//Approach-1
+#include<iostream>  
 using namespace std;
 int LengthOfString(char name[]){
 	int count=0;
@@ -12,4 +13,19 @@ int main(){
 	cout<<"enter your string:"<<endl;
 	cin>>name;
 	cout<<"Length of string:"<<LengthOfString(name);
+}
+
+//Approach-2
+#include <bits/stdc++.h>
+#include <cstring>
+using namespace std;
+int main(){
+    char ans[100];
+    cout<<"enter string:"<<endl;
+    cin>>ans;
+    int count=0;
+    for(int i=0;i<ans[i]!='\0';i++){
+        count++;
+    }
+    cout<<"Length"<<count<<endl;
 }
