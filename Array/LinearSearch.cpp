ffc1a2333b12e829087489607f5fd49fad17146d
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2023-03-10 22:37:33 +0530

    Update LinearSearch.cpp

--- Array/LinearSearch.cpp
+++ Array/LinearSearch.cpp
@@ -26,3 +26,32 @@ int main()
 	cin >> x;
 	cout << "LinearSearch:" << LinearSearch(arr, n, x);
 }
+
+//Dynamically
+#include <iostream>
+using namespace std;
+int LinearSearch(int*arr, int n, int x)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] == x)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+int main()
+{
+	int n;
+	cout << "enter number:" << endl;
+	cin >> n;
+	int*arr=new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	int x;
+	cin >> x;
+	cout << "LinearSearch:" << LinearSearch(arr, n, x);
+}
