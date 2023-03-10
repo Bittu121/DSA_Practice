e96843d45b0bd3104d94e86e057b5382fb28d662
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2023-03-10 22:39:44 +0530

    Update sum.cpp

--- Array/sum.cpp
+++ Array/sum.cpp
@@ -18,3 +18,24 @@ int main(){
 	}
 	Sum(arr,n);
 }
+
+//Dynamically
+#include<iostream>
+using namespace std;
+void Sum(int*arr,int n){
+	int sum=0;
+	for(int i=0;i<n;i++){
+		sum = sum+arr[i];
+	}
+	cout<<"Sum of all the elements :"<<sum<<" ";
+}
+int main(){
+	int n;
+	cout<<"enter number:"<<endl;
+	cin>>n;
+	int*arr=new int[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	Sum(arr,n);
+}
